2c0cd2d80a6e39c7ad05749d01f07bafe02bb1b2
Halle Williams and Ryan Streur <ryan@>
2016-05-31 16:27:27 -0700

    Totally switch approaches - switch classes to ids in the index.html and perform functions on ids instead of classes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
 $(document).ready(function() {
-  $(".definition").hide();
-  $(".concept").click(function() {
-    alert($(".concept").attr("id"));
-    var id = ($(".concept").attr("id"));
-    $(".concept#"+id).hide();
-    id = parseInt(id);
-    id += 1;
-    alert(id);
-    id = id.toString();
-    def = ".definition#"+id;
-    ($ (def).show());
+  $("#JS-def").hide();
+  $("#JS-con").click(function() {
+    $("#JS-con").hide();
+    $ ("#JS-def").show();
   });
 
-  $(".definition").click(function() {
-    $(".definition").hide();
-    $(".concept").show();
+  $("#JS-def").click(function() {
+    $("#JS-def").hide();
+    $("#JS-con").show();
+  });
+
+  $("#Operators-def").hide();
+  $("#Operators-con").click(function() {
+    $("#Operators-con").hide();
+    $("#Operators-def").show();
+  });
+
+  $("#Operators-def").click(function() {
+    $("#Operators-def").hide();
+    $("#Operators-con").show();
+  });
+
+  $("#Variables-def").hide();
+  $("#Variables-con").click(function() {
+    $("#Variables-con").hide();
+    $("#Variables-def").show();
+  });
+
+  $("#Variables-def").click(function() {
+    $("#Variables-def").hide();
+    $("#Variables-con").show();
   });
 });
